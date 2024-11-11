f7802bcab9b4bd3c50676b7dd2a0ae1d1720708d
Choi hojin <127203896+gentle-tiger@users.noreply.github.com>
2024-11-11 14:51:01 +0900

    Revert "Feat: B25 대화 애니메이션 구현"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,76 +8,43 @@ using UnityEngine.UI;
 public class GameManager :  MonoBehaviour
 {
     public TalkManager talkManager;
-    public QuestManager questManager;
-    public Animator talkPanel;
-    public Animator protraitIAnim;
+    public GameObject talkPanel;
     public Image protraitImg;
-    public Sprite prevPortrait;
-    public TypeEffect talk;
+    public TextMeshProUGUI talkText;
     public GameObject scanObject;
     public bool isAction;
-    public int talkIndex;
-
+    public int talkIndex; 
     public void Action(GameObject scanObj) 
     {
-        // Get Current Object
        scanObject = scanObj;
        ObjData objData = scanObject.GetComponent<ObjData>();
        Talk(objData.id, objData.isNpc);
 
-        // visible talk for action
-       talkPanel.SetBool("isShow", isAction);
+       talkPanel.SetActive(isAction);
     }
-        
+
     void Talk(int id, bool isNpc)
     {
-        int questTalkIndex = 0;
-        string talkData = "";
-        // Set Talk Data
-        if (talk.isAnim){ // 애니메이션 실행 중이면 
-            talk.SetMsg(""); // 다음 메시지는 실행하지 않아도 됨. 
-            return;
-        }
-        else {
-             questTalkIndex = questManager.GetQuestTalkIndex(id);
-             talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
-        }
+      string talkData = talkManager.GetTalk(id, talkIndex);
 
-        
-        // End Talk
         if (talkData == null) // 대화가 더 이상 없으면 종료
         {
             isAction = false;
             talkIndex = 0; // 이야기가 종료되면 초기화. 
-            Debug.Log(questManager.CheckQuest(id));
             return;
         }
-        // Continue Talk
+
         if (isNpc)
         {
-            // TypeEffecter 연결
-            talk.SetMsg(talkData);
+            talkText.text = talkData;
 
-            // Show Portrait
             protraitImg.color = new Color(1, 1, 1, 1); // npc라면 투명도 1(드러냄)
-
-
-            // Animation Portrait 
-            // B25 | UI 이미지가 변할 때만 움직이도록 하려면 활성화. 
-            //if (prevPortrait != protraitImg.sprite)
-            //{
-                protraitIAnim.SetTrigger("doEffect");
-                prevPortrait = protraitImg.sprite;
-            //}
         }
-        else {
-            // Hide Talk
-            talk.SetMsg(talkData);
+        else { 
+            talkText.text = talkData;
             protraitImg.color = new Color(1, 1, 1, 0); // npc가 아니라면 투명도 0(숨김) 
 
-
         }
-        // Next Talk
         isAction = true;
         talkIndex++; // 문장 뽑아내기. 
     }
